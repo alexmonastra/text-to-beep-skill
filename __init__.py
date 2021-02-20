b3749329f3d601375c3a2b425d6c74d29136c379
Alex Monastra <alexmonastra@yahoo.com>
2021-02-19 16:03:44 -0800

    play random audio on Speak message

--- __init__.py
+++ __init__.py
@@ -1,14 +1,22 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft import MycroftSkill
+from mycroft.util import play_audio_file
+import random
 
 class TextToBeep(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+    def initialize(self):
+        self.add_event("recognizer_loop:record_end", self.end_recording_sound)
+        self.add_event("speak", self.play_sound)
 
-    @intent_file_handler('beep.to.text.intent')
-    def handle_beep_to_text(self, message):
-        self.speak_dialog('beep.to.text')
-
+    print('Loading Communication Relay...')
+    
+    def end_recording_sound(self, message):
+        play_audio_file("/home/alex/mycroft-core/mycroft/res/snd/end_listening.wav")
+    
+    def play_sound(self, message):
+        number = random.randint(1,11)
+        play_audio_file(
+            "/home/alex/mycroft-core/mycroft/res/snd/bd1clips/BD-1 Clip {}.wav".format(number)
+            )
 
 def create_skill():
     return TextToBeep()
